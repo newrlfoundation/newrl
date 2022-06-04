a8a71422ef36b4cd6b8919ad9dd623c49f3270b2
Kousthub Raja <kousthub.raja@gmail.com>
2022-06-04 23:59:33 +0530

    fix mining reward credit on chain revert

--- app/migrations/init_db.py
+++ app/migrations/init_db.py
@@ -1,7 +1,8 @@
 import sqlite3
 import json
 
-from ..codes.state_updater import update_state_from_transaction
+from ..codes.blockchain import Blockchain
+from ..codes.state_updater import add_block_reward, update_state_from_transaction
 from .migrate_db import run_migrations
 from ..constants import NEWRL_DB, NEWRL_P2P_DB
 
@@ -237,17 +238,27 @@ def revert_chain(block_index):
     
     con = sqlite3.connect(NEWRL_DB)
     cur = con.cursor()
-    transactions_cursor = cur.execute(f'SELECT transaction_code, block_index, type, timestamp, specific_data FROM transactions WHERE block_index <= {block_index}').fetchall()
-    for transaction in transactions_cursor:
-        transaction_code = transaction[0]
-        block_index = transaction[1]
-        transaction_type = transaction[2]
-        timestamp = transaction[3]
-        specific_data = transaction[4]
-        while isinstance(specific_data, str):
-            specific_data = json.loads(specific_data)
+    
+    chain = Blockchain()
+    for _block_index in range(1, block_index):
+        block = chain.get_block(_block_index)
+        add_block_reward(cur=cur, creator=block['creator_wallet'], blockindex=_block_index)
+    
+        transactions_cursor = cur.execute(f'''
+            SELECT 
+            transaction_code, block_index, type, timestamp, specific_data FROM transactions 
+            WHERE block_index = {_block_index}
+            ''').fetchall()
+        for transaction in transactions_cursor:
+            transaction_code = transaction[0]
+            block_index = transaction[1]
+            transaction_type = transaction[2]
+            timestamp = transaction[3]
+            specific_data = transaction[4]
+            while isinstance(specific_data, str):
+                specific_data = json.loads(specific_data)
 
-        update_state_from_transaction(cur, transaction_type, specific_data, transaction_code, timestamp)
+            update_state_from_transaction(cur, transaction_type, specific_data, transaction_code, timestamp)
 
     con.commit()
     con.close()
