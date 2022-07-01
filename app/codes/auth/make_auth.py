bb3f6527c9cccd80acafcca9ab182921ff1af35f
Kousthub Raja <kousthub.raja@gmail.com>
2022-07-01 13:34:03 +0530

    change installation script to skip wallet input

--- app/codes/auth/make_auth.py
+++ app/codes/auth/make_auth.py
@@ -1,18 +1,20 @@
 import os.path
 import hashlib
 import json
+import argparse
 
 from ...constants import AUTH_FILE_PATH
 from ..kycwallet import generate_wallet_address
 from ..utils import get_person_id_for_wallet_address
 
 
-def make_auth_json():
+def make_auth_json(createnewwallet = False):
     if os.path.isfile(AUTH_FILE_PATH):
         print('Auth file exists already. Not creating one.')
         return
-    wallet_exists = input('Do you have an existing wallet?[Y/n]:')
-    if wallet_exists == 'Y' or wallet_exists == 'y':
+    if not createnewwallet:
+        # wallet_exists = input('Do you have an existing wallet?[Y/n]:')
+        # if wallet_exists == 'Y' or wallet_exists == 'y'):
         auth_data = json.loads(input('Paste your .auth.json file contents:\n'))
         wallet = auth_data['wallet']
         with open(AUTH_FILE_PATH, 'w') as f:
@@ -35,4 +37,8 @@ def make_auth_json():
 
 
 if __name__ == '__main__':
-    make_auth_json()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--createnewwallet", help="run the node local only with no network connection", action="store_true")
+    args = parser.parse_args()
+    
+    make_auth_json(args.createnewwallet)
