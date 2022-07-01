406e7a67c4191f59a5001efa7af8e2f19944448f
Kousthub Raja <kousthub.raja@gmail.com>
2022-07-01 13:34:03 +0530

    change installation script to skip wallet input

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,4 +5,4 @@ pip install --upgrade pip
 pip install -r requirements.txt
 python -m app.migrations.init
 python -m app.migrations.migrate_db
-python -m app.codes.auth.make_auth
+python -m app.codes.auth.make_auth --createnewwallet
