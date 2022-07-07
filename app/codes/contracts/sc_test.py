6ce911828a2fa3850a0e3e652785075ace2651dc
prannav-k <prannavk@gmail.com>
2022-07-07 23:19:56 +0530

    fixed nested type 3 txns

--- app/codes/contracts/sc_test.py
+++ app/codes/contracts/sc_test.py
@@ -10,6 +10,8 @@ class sc_test(ContractMaster):
         ContractMaster.__init__(self, self.template,self.version, address)
 
     def transfer_and_update(self,params):
+
+        #txn type 5
         transaction_creator = TransactionCreator()
         transfer_proposal_data = {
             "transfer_type": 1,
@@ -22,16 +24,31 @@ class sc_test(ContractMaster):
             "additional_data": {}
         }
         transfer_proposal = transaction_creator.transaction_type_5(transfer_proposal_data)
-    
+
+        #txn type 8
         sc_state_proposal1_data = {
             "operation": "save",
             "table_name": "sc_test",
             "sc_address": self.address,
             "data": {
-                "address": "0xc29193dbab0fe018d878e258c93064f01210ec1a",
+                "address": "0x75ff59811ba2df3c9b76d02bb156dd4a29a0dff8",
                 "total_transfer_amount": 1
             }
         }
         sc_state_proposal1 = transaction_creator.transaction_type_8(sc_state_proposal1_data)
 
-        return [transfer_proposal,sc_state_proposal1]
+        # txn tpye 3
+        sc_proposal1_params = {
+            "recipient_address": "0x20513a419d5b11cd510ae518dc04ac1690afbed6",
+            "amount": 2,
+            "tokencode": "MEME_TOKEN_DAO_token"
+        }
+        sc_proposal1_data = {
+            "address": "cte9bf57899687d0e732e7ff895aefa57e6525de35",
+            "function": 'issue_token',
+            "signers": [self.address],
+            "params": sc_proposal1_params
+        }
+        sc_proposal1 = transaction_creator.transaction_type_3(sc_proposal1_data)
+
+        return [sc_proposal1]
