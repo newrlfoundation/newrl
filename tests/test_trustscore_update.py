6eafef7cb7cd616771919e910e63bdb0c67f8bfb
Kousthub Raja <kousthub.raja@gmail.com>
2022-07-27 17:58:55 +0530

    add chain level e2e tests

---
+++ tests/test_trustscore_update.py
@@ -0,0 +1,15 @@
+import random
+import string
+import time
+import requests
+
+from setup import NODE_URL, WALLET, BLOCK_WAIT_TIME
+
+
+def test_trustscore_update():
+    
+    print('Test passed.')
+
+
+if __name__ == '__main__':
+    test_trustscore_update()
