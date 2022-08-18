e0abbaf665d023228eed6cad0102a2f8b46c826c
prannav-k <prannavk@gmail.com>
2022-08-19 02:33:02 +0530

    added tests

--- app/codes/helpers/FetchRespository.py
+++ app/codes/helpers/FetchRespository.py
@@ -1,12 +1,14 @@
+from typing import Final
+
 
 class FetchRepository:
-    __escape_string = ["--", ";--", ";", "/*", "*/", "@@", "@", "char", "nchar", "varchar", "nvarchar", "alter",
+    escape_string: Final = ["--", ";--", ";", "/*", "*/", "@@", "@", "char", "nchar", "varchar", "nvarchar", "alter",
                             "begin", "cast", "create", "cursor", "declare", "delete", "drop", "end", "exec", "execute",
                             "fetch", "insert", "kill", "select", "sys", "sysobjects", "syscolumns", "table", "update"]
-    __operation = {1: "=", 2: "in", 3: "not in",4:"like"}
+    operation: Final = {1: "=", 2: "in", 3: "not in",4:"like",5: "!="}
 
     def __init__(self, cur):
-        self.__cur = cur
+        self.cur = cur
         self.__query = ""
 
     def join_Query(self):
@@ -20,35 +22,66 @@ class FetchRepository:
         self.__query = 'Select count(' + select_param + ') from '
         return self
 
+    def select_sum(self, select_param="*"):
+        self.__query = 'Select sum(' + select_param + ') from '
+        return self
+
     def add_table_name(self, table_name):
         self.__query = self.__query + ' ' + table_name + ' '
         return self
 
-    def where_clause(self, column, value, operation=1):
+    def where_clause(self, column, value, operation):
         if self.queryCheck(column) and self.queryCheck(value):
-            self.__query = self.__query + ' where ' + column + ' ' + self.__operation[operation] + ' :' + column
+            self.__query = self.__query + ' where ' + column + ' ' + self.operation[operation] + ' :' + column
         return self
 
     def and_clause(self, column, value, operation):
         if self.queryCheck(column) and self.queryCheck(value):
-            self.__query = self.__query + ' and ' + column + ' ' + self.__operation[operation] + ' :' + column
+            self.__query = self.__query + ' and ' + column + ' ' + self.operation[operation] + ' :' + column
         return self
 
     def or_clause(self, column, value, operation):
         if self.queryCheck(column) and self.queryCheck(value):
-            self.__query = self.__query + ' or ' + column + ' ' + self.__operation[operation] + ' :' + column
+            self.__query = self.__query + ' or ' + column + ' ' + self.operation[operation] + ' :' + column
         return self
 
     def execute_query_single_result(self, queryParam):
-        return self.__cur.execute(self.__query, queryParam).fetchone()
+        return self.cur.execute(self.__query, queryParam).fetchone()
 
     def execute_query_multiple_result(self, queryParam):
-        return self.__cur.execute(self.__query, queryParam).fetchall()
+        return self.cur.execute(self.__query, queryParam).fetchall()
 
     def execute_query_update(self, queryParam):
-        return self.__cur.execute(self.__query, queryParam)
+        return self.cur.execute(self.__query, queryParam)
+
+    def insert_query_data(self, queryParam: dict):
+        keys = ','.join(queryParam.keys())
+        placeHolder = ', :'.join(queryParam.keys())
+        self.__query = self.__query + ' (' + keys + ') VALUES ( :' + placeHolder + ')'
+        return self
+    def save_private_sc_state(self, table_name: str, queryParam: dict):
+        keys = ','.join(queryParam.keys())
+        question_marks = ','.join(list('?' * len(queryParam)))
+        for x in queryParam:
+            if (not self.queryCheck(x)):
+                return False
+            if (not self.queryCheck(queryParam[x])):
+                return False
+        values = tuple(queryParam.values())
+        return self.cur.execute('INSERT INTO ' + table_name + ' (' + keys + ') VALUES (' + question_marks + ')', values)
+
+    def update_private_sc_state(self, table_name: str, queryParam: dict, whereAndParam: dict, whereOrQueryParam: dict):
+        keys = ','.join(queryParam.keys())
+        question_marks = ','.join(list('?' * len(queryParam)))
+        for x in queryParam:
+            if (not self.queryCheck(x)):
+                return False
+            if (not self.queryCheck(queryParam[x])):
+                return False
+        values = tuple(queryParam.values())
+        return self.cur.execute('UPDATE INTO ' + table_name + ' (' + keys + ') VALUES (' + question_marks + ')', values)
 
     def queryCheck(self, query: str):
-        if query in self.__escape_string:
+        if query in self.escape_string:
             return False
         return True
---
+++ tests/test_dex.py
@@ -0,0 +1,388 @@
+import logging
+import random
+import string
+import time
+import requests
+
+from setup import NODE_URL, WALLET, BLOCK_WAIT_TIME, TEST_ENV
+
+pool_token1_code = "nINR"
+pool_token2_code = "nUSDC"
+pool_ratio = "1:4"
+pool_fee = 0.005
+ot_token_code = "LPT"+''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
+ot_token_name = "LPT" + \
+    ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
+
+def create_wallet():
+# def test_create_wallet():
+    response = requests.get(NODE_URL+"/generate-wallet-address")
+    assert response.status_code == 200
+    wallet = response.json()
+    assert wallet['address']
+    assert wallet['public']
+    assert wallet['private']
+
+    response = requests.post(NODE_URL+'/add-wallet', json={
+        "custodian_address": WALLET['address'],
+        "ownertype": "1",
+        "jurisdiction": "910",
+        "kyc_docs": [
+            {
+                "type": 1,
+                "hash": "686f72957d4da564e405923d5ce8311b6567cedca434d252888cb566a5b4c401"
+            }
+        ],
+        "specific_data": {},
+        "public_key": wallet['public']
+    })
+
+    print(response.text)
+    assert response.status_code == 200
+    unsigned_transaction = response.json()
+    assert unsigned_transaction['transaction']
+    assert len(unsigned_transaction['signatures']) == 0
+
+    response = requests.post(NODE_URL+'/sign-transaction', json={
+        "wallet_data": WALLET,
+        "transaction_data": unsigned_transaction
+    })
+
+    assert response.status_code == 200
+    signed_transaction = response.json()
+    assert signed_transaction['transaction']
+    assert signed_transaction['signatures']
+    assert len(signed_transaction['signatures']) == 1
+
+    response = requests.post(
+        NODE_URL+'/validate-transaction', json=signed_transaction)
+    assert response.status_code == 200
+
+    if TEST_ENV == 'local':
+        response = requests.post(NODE_URL + '/run-updater?add_to_chain_before_consensus=true')
+    else:
+        print('Waiting to mine block')
+        time.sleep(BLOCK_WAIT_TIME)
+
+    response = requests.get(NODE_URL+'/download-state')
+    assert response.status_code == 200
+    state = response.json()
+
+    wallets = state['wallets']
+    wallet_in_state = next(
+        x for x in wallets if x['wallet_address'] == wallet['address'])
+    assert wallet_in_state
+
+    print("Wallet created with address "+wallet['address'])
+    print('Test passed.')
+    return wallet
+
+
+def create_token(wallet, owner , token_name,token_code, amount):
+    response = requests.post(NODE_URL+'/add-token', json={
+        "token_name": token_name,
+        "token_code": token_code,
+        "token_type": "1",
+        "first_owner":owner,
+        "custodian": wallet["address"],
+        "legal_doc": "686f72957d4da564e405923d5ce8311b6567cedca434d252888cb566a5b4c401",
+        "amount_created": amount,
+        "tokendecimal": 0,
+        "disallowed_regions": [],
+        "is_smart_contract_token": False,
+        "token_attributes": {}
+    })
+
+
+    assert response.status_code == 200
+    unsigned_transaction = response.json()
+    assert unsigned_transaction['transaction']
+    assert len(unsigned_transaction['signatures']) == 0
+
+    response = requests.post(NODE_URL+'/sign-transaction', json={
+        "wallet_data": wallet,
+        "transaction_data": unsigned_transaction
+    })
+
+    print("adding token")
+    assert response.status_code == 200
+    signed_transaction = response.json()
+    print("signing tx")
+    assert signed_transaction['transaction']
+    assert signed_transaction['signatures']
+    assert len(signed_transaction['signatures']) == 1
+
+    print("validating tx")
+    response = requests.post(
+        NODE_URL+'/validate-transaction', json=signed_transaction)
+    assert response.status_code == 200
+
+    if TEST_ENV == 'local':
+        response = requests.post(
+            NODE_URL + '/run-updater?add_to_chain_before_consensus=true')
+    else:
+        print('Waiting to mine block')
+        time.sleep(BLOCK_WAIT_TIME)
+
+
+
+def create_dex():
+    response_ct_add = requests.get(NODE_URL+"/generate-contract-address")
+    assert response_ct_add.status_code == 200
+    ct_address = response_ct_add.json()
+    response = requests.post(NODE_URL+'/add-sc', json=
+        {
+            "sc_address": ct_address,
+            "sc_name": "dex",
+            "version": "1.0.0",
+            "creator": WALLET['address'],
+            "actmode": "hybrid",
+            "signatories": {"initialise_liquidity": None, "provide_liquidity": None, "swap": None, "withdraw": None},
+            "contractspecs": {
+                "pool_token1_code": pool_token1_code,
+                "pool_token2_code": pool_token2_code,
+                "ot_token_code": ot_token_code,
+                "ot_token_name": ot_token_name,
+                "token_ratio": pool_ratio,
+                "fee": pool_fee
+            },
+            "legalparams": {}
+        }
+
+    )
+
+    print(response.text)
+    assert response.status_code == 200
+    unsigned_transaction = response.json()
+    assert unsigned_transaction['transaction']
+    assert len(unsigned_transaction['signatures']) == 0
+
+
+    response = requests.post(NODE_URL+'/sign-transaction', json={
+        "wallet_data": WALLET,
+        "transaction_data": unsigned_transaction
+    })
+
+    assert response.status_code == 200
+    signed_transaction = response.json()
+    assert signed_transaction['transaction']
+    assert signed_transaction['signatures']
+    assert len(signed_transaction['signatures']) == 1
+
+    response = requests.post(NODE_URL+'/validate-transaction', json=signed_transaction)
+    assert response.status_code == 200
+
+    if TEST_ENV == 'local':
+        response = requests.post(
+            NODE_URL + '/run-updater?add_to_chain_before_consensus=true')
+    else:
+        print('Waiting to mine block')
+        time.sleep(BLOCK_WAIT_TIME)
+
+    response = requests.get(NODE_URL+'/download-state')
+    assert response.status_code == 200
+    state = response.json()
+
+    contracts = state['contracts']
+    # contracts_in_state = next(
+    #     x for x in contracts if x['address'] == ct_address)
+    contracts_in_state = False
+    for x in contracts:
+        c = x['address']
+        d= ct_address
+        if c == d:
+            contracts_in_state = True
+            break
+    assert contracts_in_state 
+    return ct_address
+
+
+wallet1 = create_wallet()
+wallet1_token1_init_amount = 10000
+wallet1_token2_init_amount = 40000
+
+wallet2 = create_wallet()
+wallet2_token1_init_amount = 1000
+wallet2_token2_init_amount = 7000
+
+# def test_create_token():
+#     create_token(WALLET, wallet1['address'], pool_token1_code,
+#                  pool_token1_code, wallet1_token1_init_amount)
+
+# fund wallet1
+create_token(WALLET, wallet1['address'], pool_token1_code, pool_token1_code, wallet1_token1_init_amount)
+create_token(WALLET, wallet1['address'],pool_token2_code, pool_token2_code,wallet1_token2_init_amount)
+
+#fund wallet2
+create_token(WALLET, wallet2['address'],
+             pool_token1_code, pool_token1_code, wallet2_token1_init_amount)
+create_token(WALLET, wallet2['address'],
+             pool_token2_code, pool_token2_code, wallet2_token2_init_amount)
+
+dex_address = create_dex()
+
+def test_provide_initial_liquidity():
+    token_1_lp = 1000
+    token2_lp = 4000
+    req_json = {
+        "sc_address": dex_address,
+        "function_called": "initialise_liquidity",
+        "signers": [
+            wallet1['address']
+        ],
+        "params": {
+            "recipient_address": wallet1['address'],
+            "value": [
+                {
+                    "token_code": pool_token1_code,
+                    "amount": token_1_lp
+                },
+                {
+                    "token_code": pool_token2_code,
+                    "amount": token2_lp
+                }
+            ]
+    }
+    }
+    response = requests.post(NODE_URL+'/call-sc', json=req_json)
+
+    assert response.status_code == 200
+    unsigned_transaction = response.json()
+    assert unsigned_transaction['transaction']
+    assert len(unsigned_transaction['signatures']) == 0
+
+    response = requests.post(NODE_URL+'/sign-transaction', json={
+        "wallet_data": wallet1,
+        "transaction_data": unsigned_transaction
+    })
+
+    assert response.status_code == 200
+    signed_transaction = response.json()
+    assert signed_transaction['transaction']
+    assert signed_transaction['signatures']
+    assert len(signed_transaction['signatures']) == 1
+
+    response = requests.post(
+        NODE_URL+'/validate-transaction', json=signed_transaction)
+    assert response.status_code == 200
+
+    if TEST_ENV == 'local':
+        response = requests.post(
+            NODE_URL + '/run-updater?add_to_chain_before_consensus=true')
+    else:
+        print('Waiting to mine block')
+        time.sleep(BLOCK_WAIT_TIME)
+
+    response_token1 = requests.post(NODE_URL+'/get-balance', json={
+        "balance_type": "TOKEN_IN_WALLET",
+        "token_code": pool_token1_code,
+        "wallet_address": wallet1['address']
+    })
+    assert response_token1.status_code == 200
+    balance_token1_resp = response_token1.json()
+    balance_token1 = balance_token1_resp['balance']
+    assert balance_token1 == wallet1_token1_init_amount-token_1_lp
+
+    response_token2 = requests.post(NODE_URL+'/get-balance', json={
+        "balance_type": "TOKEN_IN_WALLET",
+        "token_code": pool_token2_code,
+        "wallet_address": wallet1['address']
+    })
+    assert response_token2.status_code == 200
+    balance_token2 = response_token2.json()['balance']
+    assert balance_token2 == wallet1_token2_init_amount-token2_lp
+
+    response_token_ot = requests.post(NODE_URL+'/get-balance', json={
+        "balance_type": "TOKEN_IN_WALLET",
+        "token_code": ot_token_code,
+        "wallet_address": wallet1['address']
+    })
+    assert response_token_ot.status_code == 200
+    balance_token_ot = response_token_ot.json()['balance']
+    assert balance_token_ot == 2000
+
+    #TODO check contract balance
+
+
+def test_swap():
+    token_1_lp = 1000
+    token2_lp = 4000
+    req_json = {
+        "sc_address": dex_address,
+        "function_called": "swap",
+        "signers": [
+            wallet2["address"]
+        ],
+        "params": {
+            "recipient_address": wallet2["address"],
+            "token_sent": {
+                "token_code": "nUSDC",
+                "amount": 30
+            },
+            "token_asked": {
+                "token_code": "NWRL"
+            },
+            "value": [
+                {
+                    "token_code": "nUSDC",
+                    "amount": 30
+                }
+            ]
+        }
+    }
+    response = requests.post(NODE_URL+'/call-sc', json=req_json)
+
+    assert response.status_code == 200
+    unsigned_transaction = response.json()
+    assert unsigned_transaction['transaction']
+    assert len(unsigned_transaction['signatures']) == 0
+
+    response = requests.post(NODE_URL+'/sign-transaction', json={
+        "wallet_data": wallet1,
+        "transaction_data": unsigned_transaction
+    })
+
+    assert response.status_code == 200
+    signed_transaction = response.json()
+    assert signed_transaction['transaction']
+    assert signed_transaction['signatures']
+    assert len(signed_transaction['signatures']) == 1
+
+    response = requests.post(
+        NODE_URL+'/validate-transaction', json=signed_transaction)
+    assert response.status_code == 200
+
+    if TEST_ENV == 'local':
+        response = requests.post(
+            NODE_URL + '/run-updater?add_to_chain_before_consensus=true')
+    else:
+        print('Waiting to mine block')
+        time.sleep(BLOCK_WAIT_TIME)
+
+    response_token1 = requests.post(NODE_URL+'/get-balance', json={
+        "balance_type": "TOKEN_IN_WALLET",
+        "token_code": pool_token1_code,
+        "wallet_address": wallet1['address']
+    })
+    assert response_token1.status_code == 200
+    balance_token1_resp = response_token1.json()
+    balance_token1 = balance_token1_resp['balance']
+    assert balance_token1 == wallet1_token1_init_amount-token_1_lp
+
+    response_token2 = requests.post(NODE_URL+'/get-balance', json={
+        "balance_type": "TOKEN_IN_WALLET",
+        "token_code": pool_token2_code,
+        "wallet_address": wallet1['address']
+    })
+    assert response_token2.status_code == 200
+    balance_token2 = response_token2.json()['balance']
+    assert balance_token2 == wallet1_token2_init_amount-token2_lp
+
+    response_token_ot = requests.post(NODE_URL+'/get-balance', json={
+        "balance_type": "TOKEN_IN_WALLET",
+        "token_code": ot_token_code,
+        "wallet_address": wallet1['address']
+    })
+    assert response_token_ot.status_code == 200
+    balance_token_ot = response_token_ot.json()['balance']
+    assert balance_token_ot == 2000
