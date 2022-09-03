46d2e72b9e37c0278783f0cc92b4746bdcb91bc1
Kousthub Raja <kousthub.raja@gmail.com>
2022-09-03 09:07:30 +0530

    retries for getting global time

--- app/codes/clock/global_time.py
+++ app/codes/clock/global_time.py
@@ -1,12 +1,24 @@
 import time
 import requests
+
+from requests.adapters import HTTPAdapter, Retry
+
 from ...constants import TIME_DIFF_WITH_GLOBAL_FILE
 
 
 def get_global_epoch():
     # url = 'http://worldclockapi.com/api/json/utc/now'
     url = 'https://worldtimeapi.org/api/timezone/Etc/UTC'
-    time_json = requests.get(url).json()
+    s = requests.Session()
+
+    retries = Retry(total=5,
+                    backoff_factor=0.1,
+                    status_forcelist=[ 500, 502, 503, 504 ])
+    s.mount('http://', HTTPAdapter(max_retries=retries))
+
+    s.get('https://worldtimeapi.org/api/timezone/Etc/UTC')
+
+    time_json = s.get(url).json()
     epoch = time_json['unixtime']
     return epoch
 
