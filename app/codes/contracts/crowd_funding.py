f99fd9fcfc3a48a85ee50bfc0d27ee2e366d459a
prannav-k <prannavk@gmail.com>
2022-10-21 13:43:03 +0530

    bug fixes

--- app/codes/contracts/crowd_funding.py
+++ app/codes/contracts/crowd_funding.py
@@ -9,7 +9,7 @@ class crowd_funding(ContractMaster):
     codehash = ""  # this is the hash of the entire document excluding this line, it is same for all instances of this class
 
     def __init__(self, contractaddress=None):
-        self.template = "dex"
+        self.template = "crowd_funding"
         self.version = ""
         ContractMaster.__init__(self, self.template,
                                 self.version, contractaddress)
@@ -17,6 +17,17 @@ class crowd_funding(ContractMaster):
     def invest(self, callparamsip, repo: FetchRepository):
         cspecs = input_to_dict(self.contractparams['contractspecs'])
         callparams = input_to_dict(callparamsip)
+
+        goal_amount = cspecs['goal_amount']
+        minimum_amount = cspecs['minimum_amount']
+
+        pledge_token_code = cspecs['pledge_token_code']
+        pledge_token_name = cspecs['pledge_token_name']
+        current_outstanding = self.get_total_issued_tokens(
+            pledge_token_code, repo)
+
+        if current_outstanding >= goal_amount:
+            raise Exception("goal amount already reached")
 
         unit_price = cspecs['unit_price']
         unit_currency = cspecs['unit_currency']
@@ -27,8 +38,8 @@ class crowd_funding(ContractMaster):
 
         amount = value[0]['amount']
         token_issue_amount = math.floor(amount/unit_price)
-        pledge_token_code = callparams['pledge_token_code']    
-        pledge_token_name = callparams['pledge_token_name']
+
+
         recipient_address = callparams['recipient_address']
 
         #TODO have new sc state to track raised amount, status, time period?
@@ -57,17 +68,18 @@ class crowd_funding(ContractMaster):
         cspecs = input_to_dict(self.contractparams['contractspecs'])
         callparams = input_to_dict(callparamsip)
 
-        pledge_token_code = callparams['pledge_token_code']
+        pledge_token_code = cspecs['pledge_token_code']
         
         company_token_name = cspecs['company_token_name']
         company_token_code = cspecs['company_token_code']
 
         recipient_address = callparams['recipient_address']
-        amount = value[0]['amount']
 
         value = callparams['value']
         if value[0]["token_code"] != pledge_token_code:
             raise Exception("Invalid tokens sent as part of value")
+
+        amount = value[0]['amount']
 
         child_transactions = []
         transaction_creator = TransactionCreator()
@@ -87,3 +99,10 @@ class crowd_funding(ContractMaster):
         child_transactions.append(
             transaction_creator.transaction_type_two(tokendata))
         return child_transactions
+
+    def get_total_issued_tokens(self,token_code,repo):
+        balance = repo.select_sum("balance").add_table_name("balances").where_clause("tokencode", token_code, 1).execute_query_single_result({"tokencode": token_code, "wallet_address": self.address})
+        if balance[0] is None:
+            return 0
+        else:    
+            return balance[0]
