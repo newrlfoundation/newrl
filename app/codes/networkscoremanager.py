edf493babf8d5ae4a6c00208c76c0bce96b4a300
Kousthub Raja <kousthub.raja@gmail.com>
2022-10-26 22:51:06 +0530

    more slashing disable

--- app/codes/networkscoremanager.py
+++ app/codes/networkscoremanager.py
@@ -103,7 +103,7 @@ def update_network_trust_score_from_receipt(cur, receipt):
                         score = get_invalid_receipt_score(existing_score)
                         logger.info('Committee member voted positive for invalid block %d. Slashing voter %s',
                             actual_block['block_index'], wallet_address)
-                        slashing_tokens(cur, wallet_address, False)
+                        # slashing_tokens(cur, wallet_address, False)
 
                     else:
                         score = get_valid_receipt_score(existing_score)
