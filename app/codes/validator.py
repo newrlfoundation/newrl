7c066cf20eef0c766ce4e007ff79a1e69ae53c4d
Kousthub Raja <kousthub.raja@gmail.com>
2022-11-24 18:13:55 +0530

    remove staleness check for transactions

--- app/codes/validator.py
+++ app/codes/validator.py
@@ -28,11 +28,11 @@ logger = logging.getLogger(__name__)
 def validate(transaction, propagate=False, validate_economics=True):
     if not validate_transaction_structure(transaction):
         return {'valid': False, 'msg': 'Invalid transaction structure'}
-    if transaction['transaction']['timestamp'] < get_corrected_time_ms() - MEMPOOL_TRANSACTION_LIFETIME_SECONDS * 1000:
-        return {
-            'valid': False,
-            'msg': 'Transaction is old'
-        }
+    # if transaction['transaction']['timestamp'] < get_corrected_time_ms() - MEMPOOL_TRANSACTION_LIFETIME_SECONDS * 1000:
+    #     return {
+    #         'valid': False,
+    #         'msg': 'Transaction is old'
+    #     }
 
     if transaction_exists_in_mempool(transaction['transaction']['trans_code']):
         return {'valid': True, 'msg': 'Already validated and in mempool', 'new_transaction': False}
