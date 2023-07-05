5f6bb1db2fe2c0e06ec20ebd041a335e237f6f41
prannav-k <prannavk@gmail.com>
2023-07-05 17:13:36 +0530

    removed migration and status

--- app/migrations/init_db.py
+++ app/migrations/init_db.py
@@ -308,8 +308,7 @@ def init_db():
                     token_code text not NULL,
                     time_updated TIMESTAMP,
                     status INT,
-                    unique_column text not NULL ,
-                    pledge_status text,
+                    unique_column text not NULL 
                     )
                     ''')
     cur.execute('''
