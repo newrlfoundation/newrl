0b662ede9375e3312288d1b85d445c5d5af8afda
prannav-k <prannavk@gmail.com>
2023-11-01 11:05:41 +0530

    moved snapshot validation to a method

--- app/core/db/dbmanager.py
+++ app/core/db/dbmanager.py
@@ -37,8 +37,7 @@ def revert_to_last_snapshot():
         snapshot = snapshots[0]
         subprocess.call(["cp", snapshot, NEWRL_DB])
 
-
-def create_block_snapshot(block_index):
+def check_snapshot_schedule(block_index):
     global snapshot_schedule
     if (
         snapshot_schedule['next_snapshot'] == -1 or 
@@ -46,28 +45,31 @@ def create_block_snapshot(block_index):
         try:
             snapshot_last_block = get_last_block_index(NEWRL_DB + '.snapshot')
         except Exception as e:
-            # logger.error('Error getting snapshot block size %s', str(e))
             snapshot_last_block = 0
         db_last_block = get_last_block_index(NEWRL_DB)
-
         if db_last_block - snapshot_last_block < 500:
-            return
+            return False
 
-        snapshot_schedule['snapshot_creation_in_progress'] = True
-        try:
-            create_db_snapshot(f'.snapshot')
-            snapshot_schedule['next_snapshot'] = block_index + random.randint(500, 1000)
-            logger.info('Next snapshot creation scheduled for block %d', snapshot_schedule['next_snapshot'])
-        except Exception as e:
-            logger.error('Error during snapshot creation' + str(e))
-        snapshot_schedule['snapshot_creation_in_progress'] = False
-        return True
+    return True    
+        
+
+def create_block_snapshot(block_index):
+    global snapshot_schedule
+    snapshot_schedule['snapshot_creation_in_progress'] = True
+    try:
+        create_db_snapshot(f'.snapshot')
+        snapshot_schedule['next_snapshot'] = block_index + random.randint(500, 1000)
+        logger.info('Next snapshot creation scheduled for block %d', snapshot_schedule['next_snapshot'])
+    except Exception as e:
+        logger.error('Error during snapshot creation' + str(e))
+    snapshot_schedule['snapshot_creation_in_progress'] = False
+    return True
     # Set the next_snapshot schedule variable based on block size
     # Next snapshot increase ~ block size
 
 
 def check_and_create_snapshot_in_thread(block_index):
-    if snapshot_schedule['snapshot_creation_in_progress']:
+    if check_snapshot(block_index):
         return False
     # thread = threading.Thread(target=create_block_snapshot,
     #     args = (block_index, ))
@@ -75,6 +77,11 @@ def check_and_create_snapshot_in_thread(block_index):
     # TODO - Check and enable threading
     return create_block_snapshot(block_index)
 
+def check_snapshot(block_index):
+    if snapshot_schedule['snapshot_creation_in_progress']:
+        return False
+    if not check_snapshot_schedule(block_index):
+        return False 
 
 def get_or_create_db_snapshot():
     snapshot_file = NEWRL_DB + '.snapshot'
